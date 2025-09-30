479bdb4689e6dbbbf3a3e3c18ccff4bff288dbe8
ramsha 1993 <rimshasaleem1993@gmail.com>
2025-09-30 16:15:02 +0500

    alignment fix

--- src/components/home/section2.jsx
+++ src/components/home/section2.jsx
@@ -79,6 +79,8 @@ const section2=()=>{
 <div class="marquee">
   <div class="marquee__inner flex items-center justify-center">
     <div class="marquee__group flex items-center justify-center">
+    <span className="h-18 w-62 flex items-center justify-center"><Image src={'/assets/home/L-2.svg'} width={200} height={80} className="  object-cover"/></span>
+
       <span className="h-18 w-62 flex items-center justify-center"><Image src={'/assets/home/L-6.svg'} width={200} height={80} className=" object-cover"/></span>
       <span className="h-18 w-62 flex items-center justify-center"><Image src={'/assets/home/L-5.svg'} width={120} height={84} className=" object-cover"/></span>
       <span className="h-18 w-62 flex items-center justify-center"><Image src={'/assets/home/L-4.svg'} width={200} height={80} className=" object-cover"/></span>
